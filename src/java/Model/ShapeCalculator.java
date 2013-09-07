53a210d880ec0d675dcdc58083c177891097d495
bit <bit@192.168.1.138>
2013-09-06 23:30:13 -0500

    Not the greatest validation, with some pretty ugly javascript. It works and I'm happy with how it looks and functions

--- src/java/Model/ShapeCalculator.java
+++ src/java/Model/ShapeCalculator.java
@@ -16,21 +16,24 @@ public class ShapeCalculator {
         switch (shape) {
             case 0: 
                 calc = new RectangleCalculator();
-                return calc.calcArea(parseValue(request.getParameter("length")), parseValue(request.getParameter("width")));
+                return calc.calcArea(parseValue(request.getParameter("length").trim()), parseValue(request.getParameter("width").trim()));
             case 1:
                 calc = new CircleCalculator();
-                return calc.calcArea(parseValue(request.getParameter("radius")), parseValue(request.getParameter("radius")));
+                return calc.calcArea(parseValue(request.getParameter("radius").trim()), parseValue(request.getParameter("radius").trim()));
             case 2:
                 calc = new TriangleCalculator();
-                return calc.calcArea(parseValue(request.getParameter("base")), parseValue(request.getParameter("height")));
+                return calc.calcArea(parseValue(request.getParameter("base".trim())), parseValue(request.getParameter("height").trim()));
             case 3:
                 calc = new TriangleCalculator();
 //                if(request.getParameter("sideC") == null || parseValue(request.getParameter("sideC")) == 0) {
 //                    return calc.calcHypotenuse(parseValue(request.getParameter("sideA")), parseValue(request.getParameter("sideC")));
 //                }
                 if(request.getParameter("sideC") == null || parseValue(request.getParameter("sideC")) == 0) {
-                    return calc.calcLegs(parseValue(request.getParameter("sideA")), parseValue(request.getParameter("sideB")));
+                    return calc.calcHypotenuse(parseValue(request.getParameter("sideA").trim()), parseValue(request.getParameter("sideB").trim()));
                 }
+                /*
+                 * NEED TO ADD LEG CALCULATIONS
+                 */
             default:
                 return 0;
         }
