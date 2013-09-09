04d11afd6630757ab15970c6e05eafd7c95450c9
bit <bit@192.168.1.138>
2013-09-09 12:18:26 -0500

    I think my architecture is bad in the model. I now understand why I can't access methods. Learning has happened here

--- src/java/Model/Calculator.java
+++ src/java/Model/Calculator.java
@@ -6,9 +6,5 @@ package Model;
  */
 public interface Calculator {
     public abstract double calcArea(double val1, double val2);
-    
-//    public abstract double calcLegs(double side, double hypotenuse);
-//    
-//    public abstract double calcHypotenuse(double sideA, double sideB);
 
 }
